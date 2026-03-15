098e541a441ff7d908635e69a41e1981dfdbee5c
Flavio Luis <46407045+flavioluis9@users.noreply.github.com>
2026-03-15 17:46:59 +0400

    Complete exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
-};
+/**
+ * Converts Fahrenheit to Celsius
+ * @param {number} f - The temperature in Fahrenheit
+ * @returns {number} - The temperature in Celsius
+ */
+function convertToCelsius(f) {
+  const celsius = (f - 32) * (5 / 9);
+  return Math.round(celsius * 10) / 10; // Rounds to 1 decimal place
+}
 
-const convertToFahrenheit = function() {
-};
+/**
+ * Converts Celsius to Fahrenheit
+ * @param {number} c - The temperature in Celsius
+ * @returns {number} - The temperature in Fahrenheit
+ */
+function convertToFahrenheit(c) {
+  const fahrenheit = (c * (9 / 5)) + 32;
+  return Math.round(fahrenheit * 10) / 10; // Rounds to 1 decimal place
+}
 
 // Do not edit below this line
 module.exports = {
