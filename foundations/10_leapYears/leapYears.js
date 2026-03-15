838a1a905e6597bb7569615d7e62e92cd2f576b5
Flavio Luis <46407045+flavioluis9@users.noreply.github.com>
2026-03-15 17:43:16 +0400

    Complete exercise 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Check if divisible by 4
+    // AND (not divisible by 100 OR is divisible by 400)
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 };
 
 // Do not edit below this line
