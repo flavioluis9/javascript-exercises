465771e8e5cdd6077b619148f10d392755945bba
Flavio Luis <46407045+flavioluis9@users.noreply.github.com>
2026-03-15 17:23:42 +0400

    Add pseudocode to exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...target) {
+
+    return array.filter(item => !target.includes(item));
+
 };
 
 // Do not edit below this line
