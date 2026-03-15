bb2d396a0c30170d2d0ba166d5b064a73578fcae
Flavio Luis <46407045+flavioluis9@users.noreply.github.com>
2026-03-15 15:38:32 +0400

    Add code to exercises 6 and 7

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(phrase, times) {
+    let repeatedString = "";
+    
+    for (i = 0; i < times; i++) {
+        repeatedString += phrase;
+    }
 
+    return repeatedString;
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = "";
+    
+    // Start at the last index and count down to 0
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    
+    return reversed;
 };
 
 // Do not edit below this line
